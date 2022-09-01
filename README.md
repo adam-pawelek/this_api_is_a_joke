49f83e72196a398b165a6cf4518bfe9d386cb233
Adam Pawelek <adam.pwk@outlook.com>
2022-09-01 20:40:50 +0000

    Connected Mysql to project

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # this_api_is_a_joke
 
 Jokes API
+
+
+### Docker-compose
+To run MySQL database open main project folder and run command
+```bash
+sudo docker-compose -f docker-compose.yml up
+```
--- src/main/java/com/example/this_api_is_a_joke/ThisApiIsAJokeApplication.java
+++ src/main/java/com/example/this_api_is_a_joke/ThisApiIsAJokeApplication.java
@@ -2,12 +2,19 @@ package com.example.this_api_is_a_joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ThisApiIsAJokeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ThisApiIsAJokeApplication.class, args);
     }
 
+    @GetMapping
+    public String hello(){
+        return "Hello world";
+    }
 }
